e1f6a642ec445de4ba53f2d694a63ce7127465a9
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2025-10-15 21:13:10 +0530

    Create Experiment1_task1.c

---
+++ Experiment1_task1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+//Task 1
+//Name
+printf("Name:-Abrarkhan Abdulgani Mokashi \n");
+//Age
+printf("Age:-17 \n");
+//Division
+printf("Division:-C \n");
+//UIN
+printf("UIN:-251P054 \n");
+return 0;
+}
