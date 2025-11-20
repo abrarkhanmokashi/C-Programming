5259764827e5fb7e914560938869f6aae1dc3a6e
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2025-11-20 23:42:38 +0530

    Create Experiment8.c

---
+++ Experiment8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);  
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
